644a5bd7da9c7c30fe26cb37688831e2d60d22f8
arxen42 <arxen42@tutanota.com>
2019-03-22 18:31:37 +0300

    Implemented IName

--- Implementation/Common/Name.cs
+++ Implementation/Common/Name.cs
@@ -5,11 +5,25 @@ namespace Practice.Common
 	/// <summary>
 	///     Скрытая реализация представления об имени человека.
 	/// </summary>
-	internal struct Name
+	internal struct Name : IName
 	{
-		/*
-		 * TODO #1: Реализуйте интерфейс IName для структуры Name
-		 */
+		public Name(String firstName, String surname, String patronymic)
+		{
+			// Так как ограничений и конкретного поведения данного класса в задании не оговорено, пока будем считать все три поля обязательными
+
+			if (String.IsNullOrWhiteSpace(firstName))
+				throw new ArgumentException(nameof(firstName));
+
+			if (String.IsNullOrWhiteSpace(surname))
+				throw new ArgumentException(nameof(firstName));
+
+			if (String.IsNullOrWhiteSpace(patronymic))
+				throw new ArgumentException(nameof(firstName));
+
+			FirstName  = firstName;
+			Surname    = surname;
+			Patronymic = patronymic;
+		}
 
 		/// <summary>
 		///     Имя.
@@ -25,5 +39,8 @@ namespace Practice.Common
 		///     Отчество.
 		/// </summary>
 		public String Patronymic { get; }
+
+		public String FullName  => $"{FirstName} {Patronymic} {Surname}";
+		public String ShortName => $"{FirstName[0]} {Patronymic[0]} {Surname}";
 	}
 }
